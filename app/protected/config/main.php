71e31f7013e3ccca3e73fcea4dd1bbc1a7d19230
Amit Malakar <amit@eatads.com>
2014-09-05 10:48:11 +0530

    replaced listing table with monitorly listing & fetch task API updated

--- app/protected/config/main.php
+++ app/protected/config/main.php
@@ -67,7 +67,7 @@ return array(
             'connectionString' => 'mysql:host=localhost;dbname=eatads',
             'emulatePrepare' => true,
             'username' => 'root',
-            'password' => 'root',
+            'password' => '',
             'charset' => 'utf8',
         ),
         'errorHandler' => array(
