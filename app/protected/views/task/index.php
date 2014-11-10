1fd950ef4f43d91571b34a8fa079a5ac4a7bbc15
chiragEA <chirag@eatads.com>
2014-11-10 17:19:51 +0530

    task redone
    task redone

--- app/protected/views/task/index.php
+++ app/protected/views/task/index.php
@@ -126,24 +126,14 @@ function scroll() {
       <div class="col-md-12">
           <h1 class="list-heading pull-left">Tasks List (<?php echo count($tasks)?>)</h1>
 <!--        <button class="btn btn-primary pull-right table-control">Save Changes</button>-->
-       
-    <table class="table table-condensed" style="table-layout:fixed">
-        <thead>
-            <tr>
+        <table class="table table-hover" id="tasks">
+         <tr> 
             <th>Campaign</th>
             <th>Site</th>
             <th>Media Type</th>
             <th>Assigned To</th>
             <th>Due Date</th>
-            </tr>
-        </thead>
-    </table>
-
-<div id="rcontent" class="div-table-content">
-
-            <table class="table table-hover" style="table-layout:fixed">
-             <tbody>
-
+        </tr>
             <?php
             foreach ($tasks as $value) {
                  $row = '<tr>
@@ -180,10 +170,7 @@ function scroll() {
                echo $row;         
             }
           ?>
-                </tbody>
-
-            </table>
-                </div>
+        </table>
       </div>
     </div>
   </div>
