a62b12c461807ceab93d69ba23a55bc4c6062e0e
Gaurav Sharma <gauravaddress@yahoo.com>
2014-11-20 16:04:08 +0530

    reports crashing

--- app/protected/models/Campaign.php
+++ app/protected/models/Campaign.php
@@ -209,9 +209,9 @@ class Campaign extends BaseCampaign {
                 $sql .= " AND t.assigneduserid IN ($assignedTo) ";
             }
              $sql .= " GROUP BY t.id ";
-          return   $sql .= " ORDER BY t.dueDate DESC";
-//            $tasks = Yii::app()->db->createCommand($sql)->queryAll();
-//            return $tasks;
+             $sql .= " ORDER BY t.dueDate DESC";
+            $tasks = Yii::app()->db->createCommand($sql)->queryAll();
+            return $tasks;
     }
     
     public static function fiterReportTasks($companyId=null, $campaignIds =null) {
